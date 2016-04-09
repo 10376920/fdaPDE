1bafa231bc152e1db392ec2c21d17787f42ddb3d
Clelia Bambini <clelia.bambini@mail.polimi.it>
2016-04-09 16:58:17 +0200

    Test added for all the 4 cases

---
+++ tests/Test_case1.R
@@ -0,0 +1,39 @@
+#TEST caso 1: COVARIATE, LOCATION NOT ON NODES
+
+librerie=c("fdaPDE_original","fdaPDE_schur")
+
+for( k in 1:2){
+	print("Test: covariates, locations not on nodes")
+	cat("Library = ", librerie[k])
+	cat("\n")
+	library = librerie[k]
+	dyn.load(paste(library, .Platform$dynlib.ext, sep = ""))
+	pathnames <- list.files(pattern="[.]R$", path="./R/", full.names=TRUE);
+	sapply(pathnames, FUN=source);
+	source("tests/mesh.R")
+
+	order = 1
+	lambda = 1
+
+	for( n in 1:7){
+		output <- mesh_quadratounitario(n*10)
+		mesh<-create.MESH.2D(nodes=output$nodes, order = order)
+		#observations_on_nodes= sin(2*pi*mesh$nodes[,1]) + rnorm(n = nrow(mesh$nodes), sd = 0.1)
+		locations <- cbind(cbind(runif(n*100*n,0,1)),cbind(runif(n*100*n,0,1)))
+		observations_not_on_nodes = sin(2*pi*locations[,1]) + rnorm(n = nrow(locations), sd = 0.1)
+		covariates = cbind(cbind(runif(n*100*n,-100,100)),cbind(runif(n*100*n,-100,100)))
+
+		FEMbasis = create.FEM.basis(mesh)
+
+		cat("n = ", n*10)
+
+		output_CPP_00 =
+		    smooth.FEM.basis(observations = observations_not_on_nodes,
+		    				locations=locations,
+		                     FEMbasis = FEMbasis,
+		                     lambda = lambda,
+		                     covariates=covariates,
+		                     GCV = TRUE,
+		                     CPP_CODE = TRUE)
+	}
+}
---
+++ tests/Test_case4.R
@@ -0,0 +1,37 @@
+#TEST caso 4: NO COVARIATE LOCATION ON NODES
+
+librerie=c("fdaPDE_original","fdaPDE_schur","fdaPDE_mumps_scotch","fdaPDE_mumps_pord")
+
+for( k in 1:4){
+	print("Test: no covariates, locations on nodes")
+	cat("Library = ", librerie[k])
+	cat("\n")
+	library = librerie[k]
+	dyn.load(paste(library, .Platform$dynlib.ext, sep = ""))
+	pathnames <- list.files(pattern="[.]R$", path="./R/", full.names=TRUE);
+	sapply(pathnames, FUN=source);
+	source("tests/mesh.R")
+
+	order = 1
+	lambda = 1
+
+	for( n in 1:7){
+		output <- mesh_quadratounitario(n*10)
+		mesh<-create.MESH.2D(nodes=output$nodes, order = order)
+		observations_on_nodes= sin(2*pi*mesh$nodes[,1]) + rnorm(n = nrow(mesh$nodes), sd = 0.1)
+		#locations <- cbind(cbind(runif(n*100*n,0,1)),cbind(runif(n*100*n,0,1)))
+		#observations_not_on_nodes = sin(2*pi*locations[,1]) + rnorm(n = nrow(locations), sd = 0.1)
+		#covariates = cbind(cbind(runif(n*100*n,-100,100)),cbind(runif(n*100*n,-100,100)))
+
+		FEMbasis = create.FEM.basis(mesh)
+
+		cat("n = ", n*10)
+
+		output_CPP_00 =
+		    smooth.FEM.basis(observations = observations_on_nodes, 
+		                     FEMbasis = FEMbasis,
+		                     lambda = lambda, 
+		                     GCV = TRUE,
+		                     CPP_CODE = TRUE)
+	}
+}
